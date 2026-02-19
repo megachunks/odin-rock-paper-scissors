4295e3195ab64d5438913ca110cc56643007e047
megachunks <megachunks@proton.me>
2026-02-19 13:52:17 -0500

    Create function to get the computer choice

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-console.log("Howdy...")
+console.log("Howdy! Let's play...");
+console.log(getComputerChoice())
 
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random() * 3)) + 1;
+    if (randomNumber === 1) return "rock"
+    if (randomNumber === 2) return "paper"
+    if (randomNumber === 3) return "scissors"
+}
