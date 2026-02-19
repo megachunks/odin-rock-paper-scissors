87b9d0c2111311f4496b7f6e1892fcce54bfa2b7
megachunks <megachunks@proton.me>
2026-02-19 14:40:20 -0500

    Create function to play a single round

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+let humanScore = 0;
+let computerScore = 0;
+
 console.log("Howdy! Let's play...");
-console.log(getComputerChoice())
+playRound()
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    console.log("Your choices human and computer:" + humanChoice + ', ' + computerChoice);
+    if (humanChoice === computerChoice) {
+        console.log("Tie! Haha... you both pick " + computerChoice);
+        return;
+    }
+    if (computerChoice === "rock") {
+        humanChoice === "paper" ? console.log("You win! Paper beats Rock") : console.log("You lose! Rock beats paper Scissors");
+        return;
+    }
+    if (computerChoice === "paper") {
+        humanChoice === "scissors" ? console.log("You win! Scissors beats Paper") : console.log("You lose! Paper beats) Rock");
+        return;
+    }
+    if (computerChoice === "scissors") {
+        humanChoice === "rock" ? console.log("You win! Rock beats Scissors") :console.log( "You lose! Scissors beats Paper");
+        return;
+    }
+}
 
 function getHumanChoice() {
     let choice = prompt("What's your choice human...?");
-    return choice;
+    return choice.toLowerCase();
 }
 
 function getComputerChoice() {
