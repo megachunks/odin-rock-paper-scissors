4e5e82c495d25c8fee969a328c68d57b7ad1b1c6
megachunks <megachunks@proton.me>
2026-02-19 13:57:48 -0500

    Create function to get the human choice

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 console.log("Howdy! Let's play...");
 console.log(getComputerChoice())
 
+function getHumanChoice() {
+    let choice = prompt("What's your choice human...?");
+    return choice;
+}
+
 function getComputerChoice() {
     let randomNumber = Math.floor((Math.random() * 3)) + 1;
     if (randomNumber === 1) return "rock"
