20626e0ffa903e48a1a0f65b2651852f1e356efc
megachunks <megachunks@proton.me>
2026-02-19 15:26:40 -0500

    Create a function to play the game and refactor
    Refactor the code of the play round function, and create win and lose
    round functions to simplify the code and remove redundancies.

--- main.js
+++ main.js
@@ -1,29 +1,11 @@
 let humanScore = 0;
 let computerScore = 0;
 
-console.log("Howdy! Let's play...");
-playRound()
-
-function playRound() {
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    console.log("Your choices human and computer:" + humanChoice + ', ' + computerChoice);
-    if (humanChoice === computerChoice) {
-        console.log("Tie! Haha... you both pick " + computerChoice);
-        return;
-    }
-    if (computerChoice === "rock") {
-        humanChoice === "paper" ? console.log("You win! Paper beats Rock") : console.log("You lose! Rock beats paper Scissors");
-        return;
-    }
-    if (computerChoice === "paper") {
-        humanChoice === "scissors" ? console.log("You win! Scissors beats Paper") : console.log("You lose! Paper beats) Rock");
-        return;
-    }
-    if (computerChoice === "scissors") {
-        humanChoice === "rock" ? console.log("You win! Rock beats Scissors") :console.log( "You lose! Scissors beats Paper");
-        return;
-    }
+function getComputerChoice() {
+    let randomNumber = Math.floor((Math.random() * 3)) + 1;
+    if (randomNumber === 1) return "rock"
+    if (randomNumber === 2) return "paper"
+    if (randomNumber === 3) return "scissors"
 }
 
 function getHumanChoice() {
@@ -31,9 +13,46 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-function getComputerChoice() {
-    let randomNumber = Math.floor((Math.random() * 3)) + 1;
-    if (randomNumber === 1) return "rock"
-    if (randomNumber === 2) return "paper"
-    if (randomNumber === 3) return "scissors"
+function winRound() {
+    console.log("You win!")
+    humanScore++;
+    return;
+}
+function loseRound() {
+    console.log("You lose!")
+    computerScore++;
+    return;
+}
+
+function playRound() {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    
+    console.log("You picked " + humanChoice + " and the computer picked " + computerChoice);
+
+    if (humanChoice === computerChoice) {
+        console.log("Tie! Haha...");
+        return;
+    }
+    if (computerChoice === "rock") {
+        humanChoice === "paper" ? winRound() : loseRound();
+    }
+    if (computerChoice === "paper") {
+        humanChoice === "scissors" ? winRound() : loseRound();
+    }
+    if (computerChoice === "scissors") {
+        humanChoice === "rock" ? winRound() : loseRound();
+    }
+    return
+}
+
+function playGame() {
+    for (let i=1; i <= 5; i++) {
+        console.log("ROUND " + i)
+        playRound();
+        console.log("Current scores human vs computer: " + humanScore + " " + computerScore);
+    }
+    if (humanScore === computerScore) console.log("GAME: ITS A TIE");
+    humanScore > computerScore ? console.log("GAME: You win!") : console.log("GAME: You lose!");
+    return;
 }
